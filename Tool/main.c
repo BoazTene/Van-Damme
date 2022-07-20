f6a546ba50179342a111fca24a09e95d14ebc201
Boaz Tene <boaztene2005@gmail.com>
2022-07-20 13:48:33 +0300

    Added base main structure

--- Tool/main.c
+++ Tool/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#include "socket.h"
+
 int main(int argc, char const *argv[])
 {
-    
+    int socket_fd = create_socket();
+
+    if (connect_socket(socket_fd, "localhost") == -1) {
+        printf("The socket connection failed.");
+
+        exit(EXIT_FAILURE);
+    } 
+
+
     return 0;
 }
