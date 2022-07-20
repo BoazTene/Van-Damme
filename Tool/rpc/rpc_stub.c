24031a4470384b1c3b3fa43651862c67ef8509ba
Boaz Tene <boaztene2005@gmail.com>
2022-07-20 15:36:25 +0300

    Create the rpc directory and the basic stubs

---
+++ Tool/rpc/rpc_stub.c
@@ -0,0 +1,70 @@
+#include "rpc_stub.h"
+
+/**
+ * @brief The main rpc function, waits for new results from the server and calls the right function. 
+ * 
+ * @param fd_socket 
+ * @return int 
+ */
+int svc_create(int fd_socket) {
+    while (1) {
+        const struct RPC rpc = recv_rpc_message(fd_socket);
+        unsigned char* result = svc_handler(rpc);
+    }
+}
+
+/**
+ * @brief Handles RPC request, calls the right function and returns the right result. 
+ * 
+ * @param rpc 
+ * @return unsigned* 
+ */
+unsigned char* svc_handler(const struct RPC rpc) {
+    printf("The %d function has been called!", rpc.function_id);
+
+    struct Result result = {status: 1, data: "hello world"};
+
+    return serialize(result);
+}
+
+/**
+ * @brief Recieves an RPC request from the server. 
+ * 
+ * @param fd_socket 
+ * @return const struct RPC 
+ */
+const struct RPC recv_rpc_message(int fd_socket) {
+    unsigned char buff[sizeof(struct RPC)];
+
+    recv(fd_socket, &buff, sizeof(buff), MSG_OOB);
+
+    return (struct RPC) deserialize(buff);
+}
+
+/**
+ * @brief Basic deserialize function for the RPC struct. 
+ * 
+ * @param buff 
+ * @return const struct RPC 
+ */
+const struct RPC deserialize(unsigned char *buff) {
+    struct RPC *rpc_message = (struct RPC*) buff;
+
+    return *rpc_message;
+}
+
+/**
+ * @brief Basic serialize function for the Result struct. 
+ * 
+ * @param result 
+ * @return unsigned* 
+ */
+unsigned char* serialize(struct Result result) {
+    ssize_t size = sizeof(struct Result);
+
+    char buff[size];
+    memset(buff, 0x00, size);
+    memcpy(buff, &result, size);
+    
+    return buff;
+}
---
+++ Tool/rpc/rpc_stub.h
@@ -0,0 +1,29 @@
+#include <sys/socket.h>
+
+/**
+ * @brief The base structure of an RPC call.
+ * 
+ * Note that the argument's size is 4092, because almost every network card supports 4096 bytes.
+ */
+struct RPC {
+    int function_id;
+    unsigned char arguments[4096 - sizeof(int)];
+};
+
+/**
+ * @brief The base structore of an RPC result. 
+ * 
+ * Note that the argument's size is 4092, becuase almost every network card supports 4096 bytes.
+ */
+struct Result {
+    int status;
+    unsigned char data[4096 - sizeof(int)]
+};
+
+/**
+ * @brief The main rpc function, waits for new results from the server and calls the right function. 
+ * 
+ * @param fd_socket 
+ * @return int 
+ */
+int svc_create(int fd_socket);
