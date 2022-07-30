e6a69a62b033fa160ac5c09ac429c56a50d41003
Boaz Tene <boaztene2005@gmail.com>
2022-07-30 16:30:38 +0300

    Fix the socket port

--- Tool/socket.c
+++ Tool/socket.c
@@ -24,11 +24,11 @@ int create_socket() {
  * @param ip The ip of the remote server, for example: 192.168.1.1 
  * @return int 0 on success, see man connect for details. 
  */
-int connect_socket(int socket_fd, char *ip) {
+int connect_socket(int socket_fd, char *ip, int port) {
     struct sockaddr_in address;
 
     address.sin_family = AF_INET;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
     inet_aton(ip, &address.sin_addr.s_addr);
 
     return connect(socket_fd, &address, sizeof(address));
